61bf97a004e9a4d7de0cbeb97079fbc69107b556
paraskhochare <paraskhochare21@gmail.com>
2026-07-28 18:53:18 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,23 +2,31 @@ export default function Navbar() {
   return (
     <header
       style={{
-        position: "sticky",
-        top: 0,
-        background: "#111827",
-        borderBottom: "1px solid #222",
-        padding: "18px 24px",
+        height: "70px",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
+        padding: "0 25px",
+        background: "#0b0715",
+        borderBottom: "1px solid #24153f",
       }}
     >
-      <h2 style={{ margin: 0 }}>AI Image Cleaner</h2>
-
-      <a
-        href="https://github.com"
+      <h2
         style={{
           color: "white",
+          margin: 0,
+        }}
+      >
+        AI Image Cleaner
+      </h2>
+
+      <a
+        href="https://github.com/paraskhochare/ai-image-cleaner"
+        target="_blank"
+        style={{
+          color: "#b794f6",
           textDecoration: "none",
+          fontWeight: 600,
         }}
       >
         GitHub
