15ba862d6238c679f6a47bacf49a41cd526156be
paraskhochare <paraskhochare21@gmail.com>
2026-07-28 19:10:09 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
+import Stats from "../components/Stats";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Features />
+      <Stats />
     </>
   );
 }
