48219108ca283702aebd608c8c89ab5fa2ad0184
paraskhochare <paraskhochare21@gmail.com>
2026-07-28 18:51:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import UploadBox from "../components/UploadBox";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <UploadBox />
     </>
   );
 }
